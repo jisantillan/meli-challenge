b173a120bdbc45e8ba4ea84adc33ab256e72bfab
jisantillan <javierignaciosantillan.u@gmail.com>
2025-03-22 03:42:48 -0300

    feat: Add req rs

---
+++ api/response/validate_melody_response.go
@@ -0,0 +1,20 @@
+package response
+
+type ValidateMelodyResponse struct {
+	Tempo Tempo  `json:"tempo"`
+	Notes []Note `json:"notes"`
+}
+
+type Tempo struct {
+	Value int    `json:"value"`
+	Unit  string `json:"unit"`
+}
+
+type Note struct {
+	Type       string  `json:"type"`
+	Name       string  `json:"name,omitempty"`
+	Octave     int     `json:"octave,omitempty"`
+	Alteration string  `json:"alteration,omitempty"`
+	Duration   float64 `json:"duration"`
+	Frequency  float64 `json:"frequency,omitempty"`
+}
